208b4963f559e6ff8df147dc28520fae38ec7fbe
anton76krikke <73478659+anton76krikke@users.noreply.github.com>
2022-01-08 13:37:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const age = (40);
+const firstName = ("Sarh");
+const totaalBedrag = (125);
+
+if (age > 17 && age < 26) {
+    console.log("je krijgt 50% korting");
+}
+else {
+    console.log("helaas geen korting voor jou");
+}
+if (firstName == "bram" || firstName == "Sarah") {
+    console.log("gefeliciteerd met je 50 ste ");
+}
+
+else {
+    console.log("nog geen 50");
+}
+
+if (totaalBedrag > 25 && totaalBedrag <= 50) {
+    console.log("gratis portie vegaballen");
+}
+else if (totaalBedrag > 50 && totaalBedrag <= 100) {
+    console.log("gratis vette nachos voor jou");
+}
+else {
+    console.log("champagne, goede bubbeljewijn");
+
+}
